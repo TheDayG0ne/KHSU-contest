c8eeb00213cb71d38492728ec388ecd32626f50a
#TheDayG0ne <thedayg0ne@internet.ru>
2023-04-01 11:49:24 +0700

    upload 1.py

---
+++ 1.py
@@ -0,0 +1,11 @@
+m, d = map(int, input().split())
+a, b = map(int, input().split())
+
+if a == b:
+    print(0)
+elif a == 0 or b == 0:
+    print(m)
+elif a %b == 0 or b %a == 0:
+    print(min(m, d))
+else:
+    print(min(m, d) * 2)
