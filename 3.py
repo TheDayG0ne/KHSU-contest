b4810c50e68f54670a7eb99c7b61f307d7bd652b
#TheDayG0ne <thedayg0ne@internet.ru>
2023-04-01 12:16:00 +0700

    upload 3.py

---
+++ 3.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+f = []
+for i in range(n):
+    x, d = map(int, input().split())
+    f.append((x, d, i))
+
+f.sort()
+ans = [0] * n
+st = ([], [])
+for x, d, i in f:
+    s = st[x%2]
+    if d == -1:
+        if s:
+            j, xj = s.pop()
+            ans[i] = ans[j] = (x - xj) // 2
+    else:
+        s.append((i, x))
+
+for t in ans: print(t)
